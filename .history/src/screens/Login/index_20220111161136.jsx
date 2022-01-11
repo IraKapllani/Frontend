5f6a051be0a891b5378875242de78591f8b7cbba
IraKapllani <79216566+IraKapllani@users.noreply.github.com>
2022-01-11 16:44:29 +0100

    stuff

---
+++ .history/src/screens/Login/index_20220111161136.jsx
@@ -0,0 +1,62 @@
+import { Textinput } from "../../components/TextInput";
+import { BiEnvelope } from 'react-icons/bi'
+import { Passwordinput } from "../../components/PasswordInput";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export function Login(props) {
+  const { setAppToken } = props;
+  const [errorMessage, setErrorMessage] = useState('');
+  const navigate = useNavigate();
+  const emailIcon = (<BiEnvelope />);
+  function submitLogin(event) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const mail = form.querySelector('input[type="text"]');
+    const password = form.querySelector('input[placeholder="Password"]');
+    const data = {};
+    data.email = mail.value;
+    data.password = password.value;
+    axios.post("http://localhost:3001/login", {
+      email: mail.value,
+      password: password.value
+    }).then(res => {
+      if (res.status === 422) {
+        
+        throw new Error('Validation failed.');
+      }
+      if (res.status !== 200 && res.status !== 201) {
+        setErrorMessage('Example error message!');
+        throw new Error('Could not authenticate you!');
+      }
+      setErrorMessage('Example error message!');
+      return res.data;
+    }).then(resData => {
+      console.log(resData);
+      localStorage.setItem("token", resData.token);
+      localStorage.setItem("userId", resData.userId);
+      setAppToken(resData.token);
+      navigate('/admin/books', {replace: true});
+    })
+  }
+
+  return (
+    <div className="login-container">
+   
+ 
+
+      <div className="login-box">
+      <p className="error"> {errorMessage} </p>
+        <form onSubmit={submitLogin} action="">
+          <h1>Log in</h1>
+          <Textinput label="Email:" icon={emailIcon} placeholder="E-mail" />
+          <Passwordinput />
+          <div className="login-div">
+          <button className="login-btn" type="submit">Log in</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ .history/src/screens/Login/index_20220111162048.jsx
@@ -0,0 +1,54 @@
+import { Textinput } from "../../components/TextInput";
+import { BiEnvelope } from 'react-icons/bi'
+import { Passwordinput } from "../../components/PasswordInput";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+export function Login(props) {
+  const { setAppToken } = props;
+  const navigate = useNavigate();
+  const emailIcon = (<BiEnvelope />);
+  function submitLogin(event) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const mail = form.querySelector('input[type="text"]');
+    const password = form.querySelector('input[placeholder="Password"]');
+    const data = {};
+    data.email = mail.value;
+    data.password = password.value;
+    axios.post("http://localhost:3001/login", {
+      email: mail.value,
+      password: password.value
+    }).then(res => {
+      if (res.status === 401) {
+        console.log('aaaaaa');
+        // throw new Error('Validation failed.');
+      }
+      if (res.status !== 200 && res.status !== 201) {
+        throw new Error('Could not authenticate you!');
+      }
+      return res.data;
+    }).then(resData => {
+      console.log(resData);
+      localStorage.setItem("token", resData.token);
+      localStorage.setItem("userId", resData.userId);
+      setAppToken(resData.token);
+      navigate('/admin/books', {replace: true});
+    })
+  }
+
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <form onSubmit={submitLogin} action="">
+          <h1>Log in</h1>
+          <Textinput label="Email:" icon={emailIcon} placeholder="E-mail" />
+          <Passwordinput />
+          <div className="login-div">
+          <button className="login-btn" type="submit">Log in</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
